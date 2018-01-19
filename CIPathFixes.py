e63cad211a4db0cfcf0648d26390f196c8b6b4ff
STELLARISHEALTH\mjanos <mjanos@wphospital.org>
2018-01-19 11:11:06 -0500

    Fixes for getting paths in CIPathFixes

--- CIPathFixes.py
+++ CIPathFixes.py
@@ -6,5 +6,5 @@ def exe_path(input_path):
     if getattr(sys,'frozen',False):
         ico_path = os.path.dirname(sys.executable)
     else:
-        ico_path = os.path.dirname(__file__)
+        ico_path = os.path.dirname(input_path)
     return os.path.join(ico_path,input_path)
